cf59ca81da8484e56849f633c0e505471fa3439d
Alan Wei <alan.wei@shopback.com>
2017-10-16 16:29:53 +0800

    clean pseudocode

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import map from 'lodash/map';
-import action from './action';
 import './style.scss';
 
 const propTypes = {
-  getUserArticles: PropTypes.func.isRequired,
-  topics: PropTypes.arrayOf(PropTypes.object).isRequired,
+  getTopics: PropTypes.func.isRequired,
 };
 
 class Home extends Component {
   componentDidMount() {
-    this.props.getUserArticles(1);
+    this.props.getTopics();
   }
-
-  renderTopics = () => (
-    map(this.props.topics, topic => (
-      <div
-        key={topic.id}
-        className="topic"
-      >
-        {topic.title}
-      </div>
-    ))
-  );
 
   render() {
     return (
@@ -38,10 +24,6 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-  getTopics: action.getTopics,
-  getUser: action.getUser,
-  getUserArticles: action.getUserArticles,
-  getUserSpecficArtile: action.getUserSpecficArtile,
 };
 
 Home.propTypes = propTypes;
--- src/views/home/reducer.js
+++ src/views/home/reducer.js
@@ -2,9 +2,6 @@ import createReducer from 'utils/createReducer';
 
 const defaultState = () => ({
   topics: [],
-  user: {},
-  articles: [],
-  errorMessage: '',
 });
 
 const getTopicsSuccess = (state, action) => ({
@@ -12,30 +9,6 @@ const getTopicsSuccess = (state, action) => ({
   topics: action.payload,
 });
 
-const getUserSuccess = (state, action) => ({
-  ...state,
-  user: action.payload,
-});
-
-const getUserError = (state, action) => ({
-  ...state,
-  errorMessage: action.payload.message,
-});
-
-const getArticlesSuccess = (state, action) => ({
-  ...state,
-  articles: action.payload,
-});
-
-const getArticlesError = (state, action) => ({
-  ...state,
-  errorMessage: action.payload.message,
-});
-
 export default createReducer(defaultState, {
   HOME_GET_TOPICS_SUCCESS: getTopicsSuccess,
-  HOME_GET_USER_SUCCESS: getUserSuccess,
-  HOME_GET_USER_ERROR: getUserError,
-  HOME_GET_ARTICLES_SUCCESS: getArticlesSuccess,
-  HOME_GET_ARTICLES_ERROR: getArticlesError,
 });

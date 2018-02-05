856d2b47811b483d120b2066382ccafab4b0e24c
Alan Wei <alan.wei@shopback.com>
2018-02-05 16:30:17 +0800

    add xss protect and cookie-parser

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const dnsPrefetchControl = require('dns-prefetch-control');
+const hidePoweredBy = require('hide-powered-by');
+const hsts = require('hsts');
+const ieNoOpen = require('ienoopen');
+const noSniff = require('dont-sniff-mimetype');
+const xssFilter = require('x-xss-protection');
 const { clientVersionMiddleware } = require('./client');
 const router = require('./router');
 
@@ -8,10 +15,19 @@ const PORT = process.env.PORT || 3000;
 const IS_PROD = ENV === 'production';
 
 const app = express();
-const logger = morgan(IS_PROD ? 'common' : 'dev');
 
-app.use(logger);
+app.use(dnsPrefetchControl({allow: false}));
+app.use(hidePoweredBy());
+app.use(hsts());
+app.use(ieNoOpen());
+app.use(noSniff());
+app.use(xssFilter({setOnOldIE: true}));
+
+app.use(cookieParser());
+app.use(morgan(IS_PROD ? 'common' : 'dev'));
+
 app.use(clientVersionMiddleware);
+
 app.get('/assets/*', (req, res) => {
   const file = req.params[0] ? req.params[0] : 'index.html';
   res.sendFile(`assets/${file}`, {
